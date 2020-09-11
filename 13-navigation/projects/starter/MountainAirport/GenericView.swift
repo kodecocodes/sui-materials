6134963eab0a62a17a198108c365f7d7134f56ef
Bill Morefield <bill@billmorefield.com>
2020-09-11 14:10:34 -0400

    Code cleanup on the starter project and added a GenericView placeholder to allow user to build project more often.

--- 13-navigation/projects/starter/MountainAirport/GenericView.swift
+++ 13-navigation/projects/starter/MountainAirport/GenericView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,35 +28,14 @@
 
 import SwiftUI
 
-struct WelcomeView: View {
-  @StateObject var flightInfo: FlightData = FlightData()
-  
-  var body: some View {
-    VStack(alignment: .leading) {
-      ZStack(alignment: .topLeading) {
-        // Background
-        Image("welcome-background")
-          .resizable()
-          .aspectRatio(contentMode: .fill)
-          .frame(width: 375, height: 250)
-          .clipped()
-        //Title
-        Text("Mountain Airport")
-          .font(.system(size: 28.0, weight: .bold))
-          .foregroundColor(.white)
-          .padding()
-      }
-      Text("Flight Status")
-      Text("Search Flights")
-      Text("Your Awards")
-      Text("Saved Flights")
-      Spacer()
-    }.font(.title)
-  }
+struct GenericView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+struct GenericView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenericView()
+    }
 }
