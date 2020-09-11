02cf82e0e1eb333cdf6b7761c2018df470afd24b
Bill Morefield <bill@billmorefield.com>
2020-09-11 11:34:17 -0400

    Converted to SwiftUI 2.0 app model. Renamed views to reflect this change. Removed code that will discussed in chapter.

--- 13-navigation/projects/starter/MountainAirport/WelcomeView.swift
+++ 13-navigation/projects/starter/MountainAirport/WelcomeView.swift
@@ -26,23 +26,38 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
 import SwiftUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+struct WelcomeView: View {
+  @StateObject var flightInfo: FlightData = FlightData()
   
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
-    // Use a UIHostingController as window root view controller
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: ContentView())
-      self.window = window
-      window.makeKeyAndVisible()
-    }
+  var body: some View {
+    VStack(alignment: .leading) {
+      ZStack(alignment: .topLeading) {
+        // Background
+        Image("welcome-background")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 375, height: 250)
+          .clipped()
+          .frame(width: 375, height: 250)
+        //Title
+        Text("Mountain Airport")
+          .font(.system(size: 28.0, weight: .bold))
+          .foregroundColor(.white)
+          .padding()
+      }
+      Text("Flight Status")
+      Text("Search Flights")
+      Text("Your Awards")
+      Text("Saved Flights")
+      Spacer()
+    }.font(.title)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    WelcomeView()
   }
 }
