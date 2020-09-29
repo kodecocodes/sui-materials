0a7738df292d197f59b9d174f348a727b300d3eb
Bill Morefield <bill@billmorefield.com>
2020-09-29 19:49:12 -0400

    Added programatic scrolling to next flight.

--- 14-lists/projects/inprogress/MountainAirport/FlightStatusBoard/FlightList.swift
+++ 14-lists/projects/inprogress/MountainAirport/FlightStatusBoard/FlightList.swift
@@ -30,15 +30,29 @@ import SwiftUI
 
 struct FlightList: View {
   var flights: [FlightInformation]
+
+  var nextFlightId: Int {
+    guard let flight = flights.first(where: { $0.localTime >= Date() }) else {
+      return flights.last!.id
+    }
+    return flight.id
+  }
   
   var body: some View {
-    List(flights) { flight in
-      NavigationLink(
-        destination: FlightDetails(flight: flight),
-        label: {
-          FlightRow(flight: flight)
+    // 1
+    ScrollViewReader { scrollProxy in
+      List(flights) { flight in
+        NavigationLink(
+          destination: FlightDetails(flight: flight),
+          label: {
+            FlightRow(flight: flight)
+          }
+        )
+      }.onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+          scrollProxy.scrollTo(nextFlightId)
         }
-      )
+      }
     }
   }
 }
