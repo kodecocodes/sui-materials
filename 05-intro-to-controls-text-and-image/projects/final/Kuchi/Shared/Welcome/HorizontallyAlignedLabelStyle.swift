7705df45e8e75bd6696a42f2f65438f42eecc10a
Antonio Bello <jeden@elapsus.com>
2020-10-12 00:27:31 +0200

    [Code] Usage Label component
    Chapter 5: "Intro to Controls: Text & Image"
    
    Xcode 12.0.1 (12A7300)

--- 05-intro-to-controls-text-and-image/projects/final/Kuchi/Shared/Welcome/HorizontallyAlignedLabelStyle.swift
+++ 05-intro-to-controls-text-and-image/projects/final/Kuchi/Shared/Welcome/HorizontallyAlignedLabelStyle.swift
@@ -30,48 +30,15 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Foundation
+
 import SwiftUI
 
-struct WelcomeView: View {
-  var body: some View {
-    ZStack {
-      Image("welcome-background", bundle: nil)
-        .resizable()
-        .aspectRatio(1 / 1, contentMode: .fill)
-        .edgesIgnoringSafeArea(.all)
-        .saturation(0.5)
-        .blur(radius: 5)
-        .opacity(0.08)
-            
-      Label {
-        VStack(alignment: .leading) {
-          Text("Welcome to")
-            .font(.headline)
-            .bold()
-          Text("Kuchi")
-            .font(.largeTitle)
-            .bold()
-        }
-        .foregroundColor(.red)
-        .lineLimit(2)
-        .multilineTextAlignment(.leading)
-        .padding(.horizontal)
-      } icon: {
-        Image(systemName: "table")
-          .resizable()
-          .frame(width: 30, height: 30)
-          .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-          .background(Color(white: 0.9))
-          .clipShape(Circle())
-          .foregroundColor(.red)
-      }
-      .labelStyle(HorizontallyAlignedLabelStyle())
+struct HorizontallyAlignedLabelStyle: LabelStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    HStack {
+      configuration.icon
+      configuration.title
     }
-  }
-}
-
-struct WelcomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
   }
 }
