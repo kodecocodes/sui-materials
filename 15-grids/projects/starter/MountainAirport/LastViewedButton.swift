d2da7fa98a4f9447564e1897df978ff0750a5c57
Bill Morefield <bill@billmorefield.com>
2020-10-30 16:25:33 -0400

    Change to reduce code needed in chapter not relevant to the topic.

--- 15-grids/projects/starter/MountainAirport/LastViewedButton.swift
+++ 15-grids/projects/starter/MountainAirport/LastViewedButton.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,19 +29,35 @@
 import SwiftUI
 
 struct LastViewedButton: View {
-  var name: String
+  @ObservedObject var flightInfo: FlightData
+  @ObservedObject var appEnvironment: AppEnvironment
+  @Binding var showNextFlight: Bool
 
   var body: some View {
-    WelcomeButtonView(
-      title: "Last Viewed Flight",
-      subTitle: name,
-      imageName: "suit.heart.fill"
-    )
+    if
+      let id = appEnvironment.lastFlightId,
+      let lastFlight = flightInfo.getFlightById(id) {
+      // swiftlint:disable multiple_closures_with_trailing_closure
+      Button(action: {
+        showNextFlight = true
+      }) {
+        WelcomeButtonView(
+          title: "Last Viewed Flight",
+          subTitle: lastFlight.flightName,
+          imageName: "suit.heart.fill"
+        )
+      }
+      // swiftlint:enable multiple_closures_with_trailing_closure
+    } else {
+      Spacer()
+    }
   }
 }
 
 struct LastViewedButton_Previews: PreviewProvider {
   static var previews: some View {
-    LastViewedButton(name: "Test Flight")
+    let environment = AppEnvironment()
+    environment.lastFlightId = 1
+    return LastViewedButton(flightInfo: FlightData(), appEnvironment: environment, showNextFlight: .constant(false))
   }
 }
