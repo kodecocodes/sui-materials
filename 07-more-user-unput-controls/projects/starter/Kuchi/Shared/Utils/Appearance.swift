cfc22444cea78d9689a5d3a48cc014c7db38effa
Antonio Bello <jeden@elapsus.com>
2020-11-21 16:38:38 +0100

    [Code] Added `name` property to `Appearance`
    Chapter 10: "More User Input and Custom Controls"
    
    Xcode 12.1.0 (12A7403)

--- 07-more-user-unput-controls/projects/starter/Kuchi/Shared/Utils/Appearance.swift
+++ 07-more-user-unput-controls/projects/starter/Kuchi/Shared/Utils/Appearance.swift
@@ -35,6 +35,13 @@ import SwiftUI
 enum Appearance: Int, CaseIterable, Identifiable {
   case light, dark, automatic
   var id: Int { self.rawValue }
+  var name: String {
+    switch self {
+    case .light: return "Light"
+    case .dark: return "Dark"
+    case .automatic: return "Automatic"
+    }
+  }
   
   func getColorScheme() -> ColorScheme? {
     switch self {
