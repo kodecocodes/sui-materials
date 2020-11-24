175d12b1fa264d9bf311c4618f501fb5460f825e
Pablo Mateo <pablo.mateofernandez@gruposantander.com>
2020-11-24 15:50:44 +0100

    Deleted extra vars in Starter project

--- 09-state-and-data-flow-part-2/projects/starter/Kuchi/Shared/Practice/PracticeView.swift
+++ 09-state-and-data-flow-part-2/projects/starter/Kuchi/Shared/Practice/PracticeView.swift
@@ -36,7 +36,7 @@ import SwiftUI
 struct PracticeView : View {
   @Binding var challengeTest: ChallengeTest?
   @Binding var userName: String
-  
+
   @ViewBuilder
   var body: some View {
     if challengeTest != nil {
@@ -48,12 +48,11 @@ struct PracticeView : View {
 }
 
 struct PracticeView_Previews : PreviewProvider {
-  @State static var numberOfAnswered: Int = 0
   static let challengeTest = ChallengeTest(
     challenge: Challenge(question: "おねがい　します", pronunciation: "Onegai shimasu", answer: "Please"),
     answers: ["Thank you", "Hello", "Goodbye"]
   )
-  
+
   static var previews: some View {
     return PracticeView(
       challengeTest: .constant(challengeTest),
