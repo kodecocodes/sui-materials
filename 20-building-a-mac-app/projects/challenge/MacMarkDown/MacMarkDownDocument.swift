b5e3519a3a47a3670088dc0d532d6100db022cc6
Sarah Reichelt <sarah@troz.net>
2021-03-26 09:10:15 +1000

    Fixed merge conflict in chapter 20 challenge

--- 20-building-a-mac-app/projects/challenge/MacMarkDown/MacMarkDownDocument.swift
+++ 20-building-a-mac-app/projects/challenge/MacMarkDown/MacMarkDownDocument.swift
@@ -34,7 +34,6 @@ import SwiftUI
 import UniformTypeIdentifiers
 import MarkdownKit
 
-<<<<<<< HEAD:20-building-a-mac-app/projects/challenge/MacMarkDown/MacMarkDownDocument.swift
 extension UTType {
   static var markdownText: UTType {
     UTType(importedAs: "net.daringfireball.markdown")
@@ -67,63 +66,18 @@ struct MacMarkDownDocument: FileDocument {
       let string = String(data: data, encoding: .utf8)
     else {
       throw CocoaError(.fileReadCorruptFile)
-=======
-struct ContentView: View {
-  @State var game = Game()
-  @State var guess: RGB
-  @State var showScore = false
-
-  var body: some View {
-    VStack {
-      Circle()
-        .fill(Color(rgbStruct: game.target))
-      if !showScore {
-        Text("R: ??? G: ??? B: ???")
-          .padding()
-      } else {
-        Text(game.target.intString())
-          .padding()
-      }
-      Circle()
-        .fill(Color(rgbStruct: guess))
-      Text(guess.intString())
-        .padding()
-      ColorSlider(value: $guess.red, trackColor: .red)
-      ColorSlider(value: $guess.green, trackColor: .green)
-      ColorSlider(value: $guess.blue, trackColor: .blue)
-      Button("Hit Me!") {
-        showScore = true
-        game.check(guess: guess)
-      }
-      .alert(isPresented: $showScore) {
-        Alert(
-          title: Text("Your Score"),
-          message: Text(String(game.scoreRound)),
-          dismissButton: .default(Text("OK")) {
-            game.startNewRound()
-            guess = RGB()
-          })
-      }
->>>>>>> editions/3.0:02-getting-started/projects/final/RGBullsEye/RGBullsEye/ContentView.swift
     }
     text = string
   }
 
-<<<<<<< HEAD:20-building-a-mac-app/projects/challenge/MacMarkDown/MacMarkDownDocument.swift
   // swiftlint:disable force_unwrapping
   func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
     let data = text.data(using: .utf8)!
     return .init(regularFileWithContents: data)
-=======
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView(guess: RGB())
->>>>>>> editions/3.0:02-getting-started/projects/final/RGBullsEye/RGBullsEye/ContentView.swift
   }
   // swiftlint:enable force_unwrapping
 }
 
-<<<<<<< HEAD:20-building-a-mac-app/projects/challenge/MacMarkDown/MacMarkDownDocument.swift
 
 extension MacMarkDownDocument {
   // swiftlint:disable indentation_width
@@ -150,18 +104,6 @@ extension MacMarkDownDocument {
       } catch {
         css = ""
       }
-=======
-struct ColorSlider: View {
-  @Binding var value: Double
-  var trackColor: Color
-
-  var body: some View {
-    HStack {
-      Text("0")
-      Slider(value: $value)
-        .accentColor(trackColor)
-      Text("255")
->>>>>>> editions/3.0:02-getting-started/projects/final/RGBullsEye/RGBullsEye/ContentView.swift
     }
 
     return """
