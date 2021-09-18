2bf845c7a80587725c16598f278ead0453db880d
Antonio Bello <1085903+jeden@users.noreply.github.com>
2021-09-18 12:30:32 +0200

    [Code] Updated the final project
    Chapter 6: "Controls & User Input"
    
    Xcode 13.0 RC (13A233)

--- 06-controls-and-user-input/projects/final/Kuchi/Shared/KuchiApp.swift
+++ 06-controls-and-user-input/projects/final/Kuchi/Shared/KuchiApp.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2020 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -39,10 +39,10 @@ struct KuchiApp: App {
   init() {
     userManager.load()
   }
-
+  
   var body: some Scene {
     WindowGroup {
-      RegisterView(keyboardHandler: KeyboardFollower())
+      RegisterView()
         .environmentObject(userManager)
     }
   }
@@ -50,6 +50,6 @@ struct KuchiApp: App {
 
 struct KuchiApp_Previews: PreviewProvider {
   static var previews: some View {
-    RegisterView(keyboardHandler: KeyboardFollower())
+    RegisterView()
   }
 }
