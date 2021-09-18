b2820a5293b521440b0d28a0eea3991c0624fda1
Antonio Bello <1085903+jeden@users.noreply.github.com>
2021-09-19 00:19:04 +0200

    [Code] Updated the starter project
    Chapter 7: "Introducing Stacks & Containers"
    
    Xcode 13.0 RC (13A233)

--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Profile/Settings.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Profile/Settings.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2020 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -31,5 +31,5 @@
 /// THE SOFTWARE.
 
 struct Settings : Codable {
-  var rememberUser: Bool = false
+    var rememberUser: Bool = false
 }
--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/StarterView.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/StarterView.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2020 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -41,7 +41,7 @@ struct StarterView: View {
       WelcomeView()
     } else {
       #if os(iOS)
-      RegisterView(keyboardHandler: KeyboardFollower())
+      RegisterView()
       #endif
       
       #if os(macOS)
