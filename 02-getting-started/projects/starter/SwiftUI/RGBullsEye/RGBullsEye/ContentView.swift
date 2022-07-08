37815c1622f7238fd3c0b9d45d5f8eae0d6c7c76
Emma Walker <walkere@tcd.ie>
2022-07-08 14:14:43 +0100

    adds stateful properties and uses them to update Colour views

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,12 +33,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var game = Game()
+    @State var guess: RGB
+    
   var body: some View {
       VStack {
-          Color(red: 0.5, green: 0.5, blue: 0.5)
+          Color(rgbStruct: game.target)
           Text("R: ??? G: ??? B: ???")
               .padding()
-          Color(red: 0.5, green: 0.5, blue: 0.5)
+          Color(rgbStruct: guess)
           Text("R: 204 G: 76 B: 178")
               .padding()
           Slider(value: .constant(0.5))
@@ -51,6 +55,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+      ContentView(guess: RGB(red: 0.8, green: 0.3, blue: 0.7))
   }
 }
--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
@@ -36,7 +36,7 @@ import SwiftUI
 struct RGBullsEyeApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+        ContentView(guess: RGB())
     }
   }
 }
