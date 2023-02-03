98e56e808fdbd2d5a8a8d01e807284f74cbda7d6
mataharimau <mataharimau@users.noreply.github.com>
2023-02-03 14:36:34 +1100

    Updated starter projects

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2023 Kodeco LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -36,7 +36,7 @@ import SwiftUI
 struct RGBullsEyeApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView(guess: RGB())
     }
   }
 }
