d2499e810c403e9b74033b29236d3ae14c65458c
mataharimau <mataharimau@users.noreply.github.com>
2023-02-05 17:25:49 +1100

    Updated for Xcode 14.2

--- 03-diving-deeper-into-swiftui/projects/final/RGBullsEye/RGBullsEye/ViewExtension.swift
+++ 03-diving-deeper-into-swiftui/projects/final/RGBullsEye/RGBullsEye/ViewExtension.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2023 Kodeco LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -33,11 +33,6 @@
 import SwiftUI
 
 extension View {
-  /// Simulate shining a light on the northwest edge of a view.
-  /// Light shadow on the northwest edge, dark shadow on the southeast edge.
-  ///   - parameters:
-  ///     - radius: The size of the shadow
-  ///     - offset: The value used for (-x, -y) and (x, y) offsets
   func northWestShadow(
     radius: CGFloat = 16,
     offset: CGFloat = 6
@@ -52,11 +47,6 @@ extension View {
         color: .shadow, radius: radius, x: offset, y: offset)
   }
 
-  /// Simulate shining a light on the southeast edge of a view.
-  /// Light shadow on the southeast edge, dark shadow on the northwest edge.
-  ///   - parameters:
-  ///     - radius: The size of the shadow
-  ///     - offset: The value used for (-x, -y) and (x, y) offsets
   func southEastShadow(
     radius: CGFloat = 16,
     offset: CGFloat = 6
