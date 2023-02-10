e65548c55fcbf16907320d2a82d338ee819818d0
Bill Morefield <bill@billmorefield.com>
2023-02-09 19:52:56 -0500

    Fixed SwiftLint warning. Updated FlightData to match chapter 15.

--- 14-lists/projects/starter/MountainAirport/Models/FlightData.swift
+++ 14-lists/projects/starter/MountainAirport/Models/FlightData.swift
@@ -228,16 +228,13 @@ class FlightData: ObservableObject {
   }
 
   static func refreshFlights() async -> [FlightInformation] {
-    Task {
-      try await Task.sleep(nanoseconds: 3 * 1_000_000_000) // Three seconds
-    }
+    sleep(3) // Three seconds
     return FlightData.generateTestFlights(date: Date())
   }
 
   static func searchFlightsForCity(_ city: String) async -> [FlightInformation] {
-    Task {
-      try await Task.sleep(nanoseconds: 3 * 1_000_000_000) // Three seconds
-    }
+    sleep(3) // Three seconds
+
     let flights = FlightData().flights
     guard !city.isEmpty else {
       return flights
