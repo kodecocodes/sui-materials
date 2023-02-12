e9e6a4d91ae562d712021a3b749def01438a4820
Antonio Bello <1085903+jeden@users.noreply.github.com>
2023-02-12 16:08:18 +0100

    [Code] Updated the starter project
    Chapter 8: "State & Data Flow — Part I"
    
    Xcode 14.2 (14C18)

--- 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ChoicesView.swift
+++ 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/Practice/ChoicesView.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2023 Kodeco Inc.
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -42,7 +42,7 @@ struct ChoicesView : View {
 
   var body: some View {
     VStack(spacing: 25) {
-      ForEach(0 ..< challengeTest.answers.count) { index in
+      ForEach(0 ..< challengeTest.answers.count, id: \.self) { index in
         Button {
           challengeSolved = checkAnswer(at: index)
           isChallengeResultAlertDisplayed = true
