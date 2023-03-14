0581efc07a7e5217b07e97490c4e54a7bf432011
Antonio Bello <1085903+jeden@users.noreply.github.com>
2023-03-14 10:05:17 +0100

    [Code] Updated the starter project
    Chapter 11: "Gestures"
    
    Xcode 14.2 (14C18)

--- 11-gestures/projects/starter/starter-chapter/Kuchi/Shared/KuchiApp.swift
+++ 11-gestures/projects/starter/starter-chapter/Kuchi/Shared/KuchiApp.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2023 Kodeco Inc.
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,9 @@ import SwiftUI
 @main
 struct KuchiApp: App {
   let userManager = UserManager()
-  @AppStorage("appearance") var appearance: Appearance = .automatic
+  @AppStorage("appearance")
+  var appearance: Appearance = .automatic
+  let challengesViewModel = ChallengesViewModel()
   
   init() {
     userManager.load()
