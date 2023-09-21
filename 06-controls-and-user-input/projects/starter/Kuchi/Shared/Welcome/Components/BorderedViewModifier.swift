3d94502f4fc1bb368fe1157120cf3bbd235a05cf
Micheal Hannan <micheal.hannan@procore.com>
2023-09-21 11:36:48 -0500

    Chapter 6: Refactor code to allow for using a custom modifier.

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/Components/BorderedViewModifier.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/Components/BorderedViewModifier.swift
@@ -32,27 +32,25 @@
 
 import SwiftUI
 
-struct RegisterView: View {
-
-    @State var name: String = ""
-
-    var body: some View {
-        VStack {
-            Spacer()
-
-            WelcomeMessageView()
-            TextField("Type your name...", text: $name)
-                .boardered()
-
-            Spacer()
-        }
-        .padding()
-        .background(WelcomeBackgroundImage())
+struct BorderedViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(color: Color.gray.opacity(0.4),
+                    radius: 3,
+                    x: 1,
+                    y: 2)
     }
 }
 
-struct RegisterView_Previews: PreviewProvider {
-    static var previews: some View {
-        RegisterView()
+extension View {
+    func boardered() -> some View {
+        ModifiedContent(content: self, modifier: BorderedViewModifier())
     }
 }
