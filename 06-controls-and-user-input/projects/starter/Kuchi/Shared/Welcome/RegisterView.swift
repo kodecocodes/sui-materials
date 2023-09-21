125b143d9ce11c0d4f9fce3154cce11e2850dae4
Micheal Hannan <micheal.hannan@procore.com>
2023-09-21 11:26:42 -0500

    Chapter 6: Creation of the custom text style applying modifiers via the textFieldStyle modifier.

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -32,6 +32,23 @@
 
 import SwiftUI
 
+struct KuchiTextStyle: TextFieldStyle {
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        return configuration
+            .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(color: Color.gray.opacity(0.4),
+                    radius: 3,
+                    x: 1,
+                    y: 2)
+    }
+}
+
 struct RegisterView: View {
 
     @State var name: String = ""
@@ -42,17 +59,7 @@ struct RegisterView: View {
 
             WelcomeMessageView()
             TextField("Type your name...", text: $name)
-                .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
-                .background(Color.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 2)
-                        .foregroundColor(.blue)
-                )
-                .shadow(color: Color.gray.opacity(0.4),
-                        radius: 3,
-                        x: 1,
-                        y: 2)
+                .textFieldStyle(KuchiTextStyle())
 
             Spacer()
         }
